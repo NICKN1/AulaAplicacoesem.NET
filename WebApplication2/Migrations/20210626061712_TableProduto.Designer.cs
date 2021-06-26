1413a6568682cacd37144c267af9f443ce4285a5
NICKN1 <iranildo.junior@outlook.com>
2021-06-26 04:12:02 -0300

    Continuação da aula

--- WebApplication2/Migrations/20210626061712_TableProduto.Designer.cs
+++ WebApplication2/Migrations/20210626061712_TableProduto.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication2.Models;
 
 namespace WebApplication2.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210626061712_TableProduto")]
+    partial class TableProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebApplication2/Models/Contexto.cs
+++ WebApplication2/Models/Contexto.cs
@@ -9,6 +9,7 @@ namespace WebApplication2.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
